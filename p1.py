e907b12d1d5aedd6315ef1a631a37c6286fbed6b
usuario prueba <prueba1@gmail.com>
2025-03-27 11:13:15 +0100

    "Otra línea fichero python 1 desde VSC rama master"

--- p1.py
+++ p1.py
@@ -1,3 +1,4 @@
 print("Fichero python 2")
 print("Agrego nueva línea fichero python 1 desde VSC")
 print("Agrego nueva línea fichero python 1 desde VSC rama master")
+print("Otra línea fichero python 1 desde VSC rama master")
